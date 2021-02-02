ed910b5097bbe7244e596b61f5a008b261a124e8
meshal <qualitymeshal@gmail.com>
2021-02-03 02:34:54 +0300

    kears model predector

--- demoapp.py
+++ demoapp.py
@@ -4,6 +4,19 @@ import numpy as np
 import investpy
 import datetime
 from datetime import datetime
+from keras.models import Sequential
+from keras .layers import Dense , LSTM ,Conv2D, Flatten
+import tensorflow as tf
+import re
+import time
+from keras.models import Sequential
+from keras .layers import Dense , LSTM ,Conv2D, Flatten
+import math
+from sklearn.preprocessing import MinMaxScaler
+
+from tensorflow.keras.models import load_model
+
+model = load_model('model.h5')
 
 
 
@@ -37,19 +50,39 @@ def only():
     start = request.form.get('startDate')
     end = request.form.get('endDate')
 
+    model = load_model('model.h5')
+
+
     start_date = datetime.strptime(start, "%Y-%m-%d").strftime("%d/%m/%Y")
     end_date = datetime.strptime(end, "%Y-%m-%d").strftime("%d/%m/%Y")
-    data = investpy.get_stock_historical_data(stock=stock, country='United States', from_date=start_date, to_date=end_date)
-    print(data.head())
+    df = investpy.get_stock_historical_data(stock=stock, country='United States', from_date=start_date, to_date=end_date)
+    df.drop('Currency',axis=1,inplace=True)
+    data = df.filter(['Close'])
+    dataset = data.values
+    scaler = MinMaxScaler(feature_range =(0,1))
+    scaled_data = scaler.fit_transform(dataset)
 
+    df = investpy.get_stock_historical_data(stock=stock, country='United States', from_date=start_date, to_date=end_date)
+    df.drop('Currency' ,axis=1,inplace=True)
+    pred_data = df.filter(['Close'])
 
+    last_60_days = pred_data[-60:].values
 
+    last_60_days_scaled = scaler.fit_transform(last_60_days)
+    X_test = []
+    X_test.append(last_60_days_scaled)
+    X_test = np.array(X_test)
+    X_test = np.reshape(X_test , (X_test.shape[0] , X_test.shape[1] , 1))
+
+    pred_price = model.predict(X_test)
+    pred_price = scaler.inverse_transform(pred_price)
+    print(pred_price)
 
     com_profile = investpy.get_stock_company_profile(stock=stock, country='United States', language='english')
     profile = com_profile['desc']
 
 
-    return render_template('only.html' , stock= stock , start_date=start_date , end_date=end_date,profile=profile  )
+    return render_template('only.html' , stock= stock , start_date=start_date , end_date=end_date,profile=profile , pred_price=pred_price  )
 
 
 
--- test.py
+++ test.py
@@ -3,6 +3,20 @@ import pandas as pd
 import numpy as np 
 import datetime
 from datetime import datetime
+from keras.models import Sequential
+from keras .layers import Dense , LSTM ,Conv2D, Flatten
+import tensorflow as tf
+import re
+import time
+from keras.models import Sequential
+from keras .layers import Dense , LSTM ,Conv2D, Flatten
+import math
+from sklearn.preprocessing import MinMaxScaler
+
+from keras.models import load_model
+import os
+from keras.models import model_from_json
+from tensorflow.keras.models import load_model
 '''
 print ('ok')
 
@@ -16,7 +30,7 @@ print(len(data))
 print(data['Gross Profit'])
 #print(data['Gross Profit'].index[0])
 #data = investpy.get_stock_historical_data(stock='bbva', country='spain', from_date='01/01/2010', to_date='01/01/2019')
-'''
+
 start = '2021-02-03'
 start_date = datetime.strptime(start, "%Y-%m-%d").strftime("%d/%m/%Y")
 print('The Start Date is')
@@ -26,4 +40,11 @@ print('The Type is ')
 print(type(start_date))
 print('------------------------')
 
+df.drop('Currency',axis=1,inplace=True)
+'''
 
+from tensorflow.keras.models import load_model
+
+model = load_model('model.h5')
+
+print('ok')
