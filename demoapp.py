7be58a83a66a783169e1d8220db05f5dfdc5a980
meshal <qualitymeshal@gmail.com>
2021-02-03 00:49:05 +0300

    price parce by fix the date code

--- demoapp.py
+++ demoapp.py
@@ -2,6 +2,8 @@ from flask import Flask,url_for ,request, render_template
 import pandas as pd 
 import numpy as np 
 import investpy
+import datetime
+from datetime import datetime
 
 
 
@@ -23,21 +25,31 @@ def temp():
     start_date = request.form.get('startDate')
     end_date = request.form.get('endDate')
 
+
+
     
     
-    return render_template('home.html' , stock= stock , start_date=start_date , end_date=end_date )
+    return render_template('home.html')
 
 @app.route('/only' , methods=['GET' , 'POST'])
 def only():
     stock = request.form.get('stockName').upper()
-    start_date = request.form.get('startDate')
-    end_date = request.form.get('endDate')
-    
+    start = request.form.get('startDate')
+    end = request.form.get('endDate')
+
+    start_date = datetime.strptime(start, "%Y-%m-%d").strftime("%d/%m/%Y")
+    end_date = datetime.strptime(end, "%Y-%m-%d").strftime("%d/%m/%Y")
+    data = investpy.get_stock_historical_data(stock=stock, country='United States', from_date=start_date, to_date=end_date)
+    print(data.head())
+
+
+
+
     com_profile = investpy.get_stock_company_profile(stock=stock, country='United States', language='english')
     profile = com_profile['desc']
 
 
-    return render_template('only.html' , stock= stock , start_date=start_date , end_date=end_date,profile=profile )
+    return render_template('only.html' , stock= stock , start_date=start_date , end_date=end_date,profile=profile  )
 
 
 
--- test.py
+++ test.py
@@ -1,9 +1,29 @@
 import investpy
 import pandas as pd
 import numpy as np 
-
+import datetime
+from datetime import datetime
+'''
 print ('ok')
 
 profile = investpy.get_stock_company_profile(stock='AAPL', country='United States', language='english')
 
 print (profile['desc'])
+
+data = investpy.get_stock_financial_summary(stock='AAPL', country='United States', summary_type='income_statement', period='annual')
+print(len(data))
+#print(data.columns)
+print(data['Gross Profit'])
+#print(data['Gross Profit'].index[0])
+#data = investpy.get_stock_historical_data(stock='bbva', country='spain', from_date='01/01/2010', to_date='01/01/2019')
+'''
+start = '2021-02-03'
+start_date = datetime.strptime(start, "%Y-%m-%d").strftime("%d/%m/%Y")
+print('The Start Date is')
+print(start_date)
+print('------------------------')
+print('The Type is ')
+print(type(start_date))
+print('------------------------')
+
+
